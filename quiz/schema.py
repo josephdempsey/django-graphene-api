e52e49b333c786d73539d172356b7babdcd13f33
Dempsey Joe <dempseyjoe@dvl-fm52lml85.mynet>
2021-06-11 16:08:38 +0100

    feat(initialise quiz app): first graphQl query with quiz app

---
+++ quiz/schema.py
@@ -0,0 +1,33 @@
+import graphene
+from graphene_django import DjangoObjectType
+from graphene_django import DjangoListField
+from .models import Quizzes, Category, Question, Answer
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = ("id","name")
+
+class QuizzesType(DjangoObjectType):
+    class Meta:
+        model = Quizzes
+        fields = ("id","title","category","quiz")
+
+class QuestionType(DjangoObjectType):
+    class Meta:
+        model = Question
+        fields = ("title","quiz")
+
+class AnswerType(DjangoObjectType):
+    class Meta:
+        model = Answer
+        fields = ("question","answer_text")
+
+class Query(graphene.ObjectType):
+
+    quiz = graphene.String()
+
+    def resolve_quiz(root, info):
+        return f"This is the first question"
+
+schema = graphene.Schema(query=Query)
